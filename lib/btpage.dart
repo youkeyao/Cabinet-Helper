81c2678389205fe8d236b6e366b7fda6203f83f9
youkeyao <youkeyao@sjtu.edu.cn>
2021-09-21 14:46:36 +0800

    flutter_reactive_ble

--- lib/btpage.dart
+++ lib/btpage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_blue/flutter_blue.dart';
-
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'btutil.dart';
 
 class BTPage extends StatefulWidget{
@@ -13,8 +12,10 @@ class BTPage extends StatefulWidget{
 }
 
 class BTPageState extends State<BTPage>{
-  Set<BluetoothDevice> devices = {};
-  String status = BtUtil.btdevice == null ? '未连接' : '已连接';
+  List devices = [];
+  List ids = [];
+  List uuids = [];
+  String status = BtUtil.id == "" ? '未连接' : '已连接';
 
   @override
   Widget build(BuildContext context) {
@@ -24,22 +25,22 @@ class BTPageState extends State<BTPage>{
         leading: const Icon(Icons.bluetooth),
         title: Text(status),
         centerTitle: true,
-        actions: [IconButton(onPressed: scanDevice, icon: const Icon(Icons.refresh))],
+        actions: [IconButton(onPressed: () => write(BtUtil.id, BtUtil.uuid[0]), icon: const Icon(Icons.refresh))],
       ),
       body: ListView(
-        children: devices.map((d) => TextButton(
+        children: devices.asMap().keys.map((i) => TextButton(
           style: ButtonStyle(
             padding: MaterialStateProperty.all(const EdgeInsets.symmetric(vertical: 18,horizontal: 15)),
-            foregroundColor: MaterialStateProperty.all(d == BtUtil.btdevice ? Colors.blue : Colors.grey),
+            foregroundColor: MaterialStateProperty.all(ids[i] == BtUtil.id ? Colors.blue : Colors.grey),
           ),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(d.name == "" ? "unknown" : d.name, style: const TextStyle(fontSize: 18.0,),),
-              Text(d.id.toString(), style: const TextStyle(fontSize: 18.0,),),
+              Text(devices[i] == "" ? 'unknown' : devices[i], style: const TextStyle(fontSize: 18.0,),),
+              Text(ids[i], style: const TextStyle(fontSize: 18.0,),),
             ],
           ),
-          onPressed: () => pressDevice(d),
+          onPressed: () => pressDevice(devices[i], ids[i], uuids[i]),
         )).toList(),
       ),
     );
@@ -48,42 +49,73 @@ class BTPageState extends State<BTPage>{
   @override
   void initState() {
     super.initState();
-    BtUtil.flutterBlue.scanResults.listen((results) {
-      for (ScanResult r in results) {
-        setState(() {
-          devices.add(r.device);
-        });
-      }
+    BtUtil.flutterReactiveBle.statusStream.listen((e) {
+      print('mmmmmmmmmmmmmmmmmstatus' + e.toString());
     });
+    
     scanDevice();
   }
 
   void scanDevice() {
     setState(() {
       devices.clear();
-      if (BtUtil.btdevice != null) devices.add(BtUtil.btdevice!);
+      ids.clear();
+      uuids.clear();
+      if (BtUtil.id != "") {
+        devices.add(BtUtil.name);
+        ids.add(BtUtil.id);
+        uuids.add(BtUtil.uuid);
+      }
     });
-    BtUtil.scanDevice();
+    BtUtil.flutterReactiveBle.scanForDevices(withServices: [], scanMode: ScanMode.lowLatency).listen((device) {
+      setState(() {
+        if (!ids.contains(device.id)) {
+          devices.add(device.name);
+          ids.add(device.id);
+          uuids.add(device.serviceUuids);
+        }
+      });
+    }, onError: (on) {
+      print('scan error');
+    });
   }
 
-  void pressDevice(BluetoothDevice d) async {
-    setState(() {
-      status = '连接中';
-    });
-    if (d == BtUtil.btdevice) {
-      await BtUtil.disconnectDevice(d);
-    }
-    else {
-      await BtUtil.connectDevice(d);
-    }
-    setState(() {
-      if (BtUtil.btdevice != null) {
-        status = '已连接';
+  void pressDevice(String name, String id, List uuid) {
+    BtUtil.flutterReactiveBle.connectToDevice(
+      id: id,
+      connectionTimeout: const Duration(seconds: 2),
+    ).listen((connectionState) {
+      if (connectionState.connectionState == DeviceConnectionState.connecting) {
+        setState(() {
+          status = '连接中';
+        });
       }
-      else {
-        status = '未连接';
+      else if (connectionState.connectionState == DeviceConnectionState.connected) {
+        setState(() {
+          status = '已连接';
+        });
+        BtUtil.name = name;
+        BtUtil.id = id;
+        BtUtil.uuid = uuid;
+        scanDevice();
       }
+      else if (connectionState.connectionState == DeviceConnectionState.disconnected) {
+        setState(() {
+          status = '未连接';
+        });
+        BtUtil.name = "";
+        BtUtil.id = "";
+      }
+    }, onError: (Object error) {
+      // handle error
     });
-    scanDevice();
+  }
+
+  void write(String id, Uuid uuid) async {
+    List<DiscoveredService> serviceList = await BtUtil.flutterReactiveBle.discoverServices(id);
+    final characteristic = QualifiedCharacteristic(serviceId: serviceList[0].serviceId, characteristicId: serviceList[0].characteristicIds[0], deviceId: id); 
+    await BtUtil.flutterReactiveBle.writeCharacteristicWithResponse(characteristic, value: [65, 66]);
+    final response = await BtUtil.flutterReactiveBle.readCharacteristic(characteristic);
+    print("response=$response");
   }
 }
--- lib/btutil.dart
+++ lib/btutil.dart
@@ -1,44 +1,17 @@
-import 'package:flutter_blue/flutter_blue.dart';
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 class BtUtil{
-  static BluetoothDevice? btdevice;
-  static FlutterBlue flutterBlue = FlutterBlue.instance;
-  static BluetoothCharacteristic? btcha;
+  static String name = "";
+  static String id = "";
+  static List uuid = [];
+  static final flutterReactiveBle = FlutterReactiveBle();
 
-  static void scanDevice() {
-    flutterBlue.startScan(timeout: const Duration(seconds: 2));
-    flutterBlue.stopScan();
-  }
-
-  static Future connectDevice(BluetoothDevice d) async {
-    await d.connect(autoConnect: false, timeout: const Duration(seconds: 10));
-    btdevice = d;
-
-    List<BluetoothService> services = await d.discoverServices();
-    services.map((service) {
-      if (service.uuid.toString().toUpperCase().substring(4, 8) == "FFF0") {
-        List<BluetoothCharacteristic> characteristics = service.characteristics;
-        characteristics.map((characteristic) {
-          var valuex = characteristic.uuid.toString();
-          print("所有特征值 --- $valuex");
-          if (service.uuid.toString().toUpperCase().substring(4, 8) == 'FFF1') {
-            print("匹配到正确的特征值");
-            
-            btcha = characteristic;
-          }
-        });
-      }
-    });
-    await btcha?.setNotifyValue(true);
-    btcha?.value.listen((value) {
-      print('hello'+value.toString());
-    });
+  static Future connectDevice(DiscoveredDevice d) async {
+    
 
     return true;
   }
 
-  static Future disconnectDevice(BluetoothDevice d) async {
-    await d.disconnect();
-    btdevice = null;
+  static Future disconnectDevice(DiscoveredDevice d) async {
     return true;
   }
 }
